dadc18fb16e7b95350197c0fe58d0c2586de43a4
Rob Whitton <limehc@gmail.com>
2020-05-30 21:36:34 +0100

    add change facing direction sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
@@ -26,5 +27,22 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //-1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        //if player moves horizontally
+
+        //mathf.Abs - if float is +ve or 0, returns 1, else returns -1
+        bool playerHasHorizonalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizonalSpeed)
+        {
+            //reverse current scaling of x axis
+
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f); //uses the velocity on x (so it will be positive if going right, negative if going left), then setting that to the localScale
+        }
+
+
     }
 }
