c869f7352c7b451cb610f824041b890296b50146
Rob Whitton <limehc@gmail.com>
2020-05-31 21:36:01 +0100

    make jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,24 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    //Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
+    //State
+    bool isAlive = true;
+
+    //cached component references
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
+    //messages, then methods
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,21 +30,36 @@ public class Player : MonoBehaviour
     {
         Run();
         FlipSprite();
+        Jump();
     }
 
     private void Run()
     {
+        //the cross platform input manager lets us make our games for different devices
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //-1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        Debug.Log("player velocity :" + playerVelocity);
+
+        myAnimator.SetBool("running", PlayerHasHorizontalSpeed());
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump")) //from project settings -> input
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
     {
         //if player moves horizontally
 
-        //mathf.Abs - if float is +ve or 0, returns 1, else returns -1
-        bool playerHasHorizonalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        
+        bool playerHasHorizonalSpeed = PlayerHasHorizontalSpeed();
 
         if (playerHasHorizonalSpeed)
         {
@@ -44,5 +69,11 @@ public class Player : MonoBehaviour
         }
 
 
+    }
+
+    private bool PlayerHasHorizontalSpeed()
+    {
+        //mathf.Abs - if float is +ve or 0, returns 1, else returns -1
+        return Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
     }
 }
